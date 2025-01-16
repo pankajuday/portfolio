160cdef09bd106901922914a7b1c26fd98daa692
pankaj <pankajuday7033@gmail.com>
2025-01-16 16:17:42 +0530

    updated backend :: >>> added vercel.json

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:3000",
+        target: "portfolio-server-pankaj.vercel.app",
         changeOrigin: true,
       },
     },
